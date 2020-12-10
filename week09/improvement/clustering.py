a6ec0a72e1f7842287f20dce63446c856474db9d
Umit <60366307+umitozmen@users.noreply.github.com>
2020-12-10 12:50:15 +0000

    Half Way Inclass exercise @jri-19 @zcapjdb

--- week09/improvement/clustering.py
+++ week09/improvement/clustering.py
@@ -3,30 +3,33 @@
 from math import *
 from random import *
 
-k=3
+k_means_clusters=3
+number_of_ttl_iterations = 10
 
 lines = open('samples.csv', 'r').readlines()
-ps=[]
-for line in lines: ps.append(tuple(map(float, line.strip().split(','))))
+points=[]
 
-m=[ps[randrange(len(ps))], ps[randrange(len(ps))], ps[randrange(len(ps))]]
+for line in lines: points.append(tuple(map(float, line.strip().split(','))))
 
-alloc=[None]*len(ps)
+means =[]
+for m in range(k_means_clusters): means.append(points[randrange(len(points))])
+
+alloc=[None]*len(points)
+
 n=0
-while n<10:
-  for i in range(len(ps)):
-    p=ps[i]
-    d=[None] * 3
-    d[0]=sqrt((p[0]-m[0][0])**2 + (p[1]-m[0][1])**2)
-    d[1]=sqrt((p[0]-m[1][0])**2 + (p[1]-m[1][1])**2)
-    d[2]=sqrt((p[0]-m[2][0])**2 + (p[1]-m[2][1])**2)
+while n < number_of_ttl_iterations:
+  for i,p in enumerate(points):
+    d=[None] * k_means_clusters
+    d[0]=sqrt((p[0]-means[0][0])**2 + (p[1]-means[0][1])**2)
+    d[1]=sqrt((p[0]-means[1][0])**2 + (p[1]-means[1][1])**2)
+    d[2]=sqrt((p[0]-means[2][0])**2 + (p[1]-means[2][1])**2)
     alloc[i]=d.index(min(d))
-  for i in range(3):
-    alloc_ps=[p for j, p in enumerate(ps) if alloc[j] == i]
+  for i in range(k_means_clusters):
+    alloc_ps=[p for j, p in enumerate(points) if alloc[j] == i]
     new_mean=(sum([a[0] for a in alloc_ps]) / len(alloc_ps), sum([a[1] for a in alloc_ps]) / len(alloc_ps))
     m[i]=new_mean
   n=n+1
 
-for i in range(3):
+for i in range(k_means_clusters):
   alloc_ps=[p for j, p in enumerate(ps) if alloc[j] == i]
   print("Cluster " + str(i) + " is centred at " + str(m[i]) + " and has " + str(len(alloc_ps)) + " points.")
